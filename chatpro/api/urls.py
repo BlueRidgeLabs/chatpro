39a1f232f2eb172d43efbf13a0aa6a03a9f39eed
Rowan Seymour <rowanseymour@gmail.com>
2015-01-21 11:45:11 +0200

    Fixed some PEP violations

--- chatpro/api/urls.py
+++ chatpro/api/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls import patterns, url
 from .views import TembaHandler
 
 urlpatterns = patterns('',
-                       url(r'^/(?P<entity>message|contact)/(?P<action>new|del)/',
+                       url(r'^(?P<entity>message|contact)/(?P<action>new|del)/',
                            TembaHandler.as_view(),
                            name='api.temba_handler'))
--- chatpro/urls.py
+++ chatpro/urls.py
@@ -4,21 +4,19 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'', include('chatpro.home.urls')),
-    url(r'', include('chatpro.msgs.urls')),
-    url(r'', include('chatpro.profiles.urls')),
-    url(r'', include('chatpro.rooms.urls')),
-    url(r'^manage/', include('chatpro.orgs_ext.urls')),
-    url(r'^users/', include('dash.users.urls')),  # TODO replace forget password views and remove
-    url(r'^api/v1', include('chatpro.api.urls')),
-    url(r'^i18n/', include('django.conf.urls.i18n')),
-)
+                       url(r'', include('chatpro.home.urls')),
+                       url(r'', include('chatpro.msgs.urls')),
+                       url(r'', include('chatpro.profiles.urls')),
+                       url(r'', include('chatpro.rooms.urls')),
+                       url(r'^manage/', include('chatpro.orgs_ext.urls')),
+                       url(r'^api/v1/', include('chatpro.api.urls')),
+                       url(r'^users/', include('dash.users.urls')),
+                       url(r'^i18n/', include('django.conf.urls.i18n')))
 
 if settings.DEBUG:  # pragma: no cover
     import debug_toolbar
     urlpatterns = patterns('',
-    url(r'^__debug__/', include(debug_toolbar.urls)),
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-    url(r'', include('django.contrib.staticfiles.urls')),
-) + urlpatterns
+                           url(r'^__debug__/', include(debug_toolbar.urls)),
+                           url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+                               {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+                           url(r'', include('django.contrib.staticfiles.urls'))) + urlpatterns
