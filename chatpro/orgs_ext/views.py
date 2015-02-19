d4d474be71db85a99fde6ca54ab2bedc5e2ed332
Rowan Seymour <rowanseymour@gmail.com>
2015-02-19 17:33:26 +0200

    Contacts without URNs counted as failed during sync

--- chatpro/orgs_ext/views.py
+++ chatpro/orgs_ext/views.py
@@ -1,10 +1,13 @@
 from __future__ import absolute_import, unicode_literals
 
+from chatpro.orgs_ext import TaskType
 from dash.orgs.models import Org
 from dash.orgs.views import OrgCRUDL, InferOrgMixin, OrgPermsMixin, SmartUpdateView
+from dash.utils import ms_to_datetime
 from django.core.urlresolvers import reverse
 from django import forms
 from django.utils.translation import ugettext_lazy as _
+from smartmin.templatetags.smartmin import format_datetime
 from smartmin.users.views import SmartCRUDL
 
 
@@ -28,7 +31,7 @@ class OrgExtCRUDL(SmartCRUDL):
         pass
 
     class Home(OrgCRUDL.Home):
-        fields = ('name', 'api_token', 'chat_name_field', 'new_message_webhook', 'new_contact_webhook', 'delete_contact_webhook')
+        fields = ('name', 'api_token', 'chat_name_field', 'last_contact_sync', 'new_message_webhook', 'new_contact_webhook', 'delete_contact_webhook')
         field_config = {'api_token': {'label': _("RapidPro API Token")}}
         permission = 'orgs.org_home'
 
@@ -37,6 +40,17 @@ class OrgExtCRUDL(SmartCRUDL):
 
         def get_chat_name_field(self, obj):
             return obj.get_chat_name_field()
+
+        def get_last_contact_sync(self, obj):
+            result = obj.get_task_result(TaskType.sync_contacts)
+            if result:
+                return "%s (%d created, %d updated, %d deleted, %d failed)" % (format_datetime(ms_to_datetime(result['time'])),
+                                                                               result['counts']['created'],
+                                                                               result['counts']['updated'],
+                                                                               result['counts']['deleted'],
+                                                                               result['counts']['failed'])
+            else:
+                return None
 
         def get_new_message_webhook(self, obj):
             return build_webhook(obj, self.request, 'message', 'new', 'contact=@contact.uuid&text=@step.value&group=[GROUP_UUID]')
--- chatpro/utils/tests.py
+++ chatpro/utils/tests.py
@@ -25,12 +25,10 @@ class TembaTest(ChatProTest):
                                 fields=dict(chat_name="eve"), language='eng', modified_on=timezone.now())
         ]
 
-        room_uuids = [r.uuid for r in self.unicef.rooms.all()]
+        created, updated, deleted, failed = sync_pull_contacts(self.unicef, Contact)
+        self.assertFalse(created or updated or deleted or failed)
 
-        created, updated, deleted = sync_pull_contacts(self.unicef, room_uuids, Contact)
-        self.assertFalse(created or updated or deleted)
-
-        # RapidPro returning 1 new, 1 modified and 1 deleted contact
+        # RapidPro returning 1 new, 1 modified, 1 deleted and 2 unsyncable contact
         mock_get_contacts.return_value = [
             TembaContact.create(uuid='C-001', name="Annie", urns=['tel:5678'], groups=['G-002'],
                                 fields=dict(chat_name="annie"), language='eng', modified_on=timezone.now()),
@@ -41,13 +39,18 @@ class TembaTest(ChatProTest):
             TembaContact.create(uuid='C-005', name="Eve", urns=['twitter:evee'], groups=['G-003'],
                                 fields=dict(chat_name="eve"), language='eng', modified_on=timezone.now()),
             TembaContact.create(uuid='C-007', name="Jim", urns=['twitter:jimbo'], groups=['G-003'],
-                                fields=dict(chat_name="jim"), language='eng', modified_on=timezone.now())
+                                fields=dict(chat_name="jim"), language='eng', modified_on=timezone.now()),
+            TembaContact.create(uuid='C-009', name="Xen", urns=['twitter:xen'], groups=[],  # no room group
+                                fields=dict(chat_name="xen"), language='eng', modified_on=timezone.now()),
+            TembaContact.create(uuid='C-010', name="Yan", urns=[], groups=['G-003'],  # no URN
+                                fields=dict(chat_name="yan"), language='eng', modified_on=timezone.now())
         ]
 
-        created, updated, deleted = sync_pull_contacts(self.unicef, room_uuids, Contact)
+        created, updated, deleted, failed = sync_pull_contacts(self.unicef, Contact)
         self.assertEqual(sorted(created), ['C-007'])
         self.assertEqual(sorted(updated), ['C-001'])
         self.assertEqual(sorted(deleted), ['C-002'])
+        self.assertEqual(sorted(failed), ['C-009', 'C-010'])
 
         # check created contact
         jim = Contact.objects.get(uuid='C-007')
